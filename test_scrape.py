7131eb19bbcdf49418610eeb48150bdb088ba762
lukewiwa <luke.wiwa@gmail.com>
2020-02-09 22:47:01 +1100

    Add poetry and use formatter

--- test_scrape.py
+++ test_scrape.py
@@ -1,7 +1,8 @@
 import pytest
 import os
-from scrape import Code
 import lxml.etree as ET
+from scrape import Code
+
 
 class Code_test(Code):
     def __init__(self, xml):
@@ -10,11 +11,12 @@ class Code_test(Code):
 
 
 def test_element_path_false():
-    xml = '<Total><test><b>A</b></test></Total>'
+    xml = "<Total><test><b>A</b></test></Total>"
     code = Code_test(xml)
     assert code.getGH(code.root) is None
 
+
 def test_element_path_true():
-    xml = '<Total><test><b>H</b></test></Total>'
+    xml = "<Total><test><b>H</b></test></Total>"
     code = Code_test(xml)
     assert code.getGH(code.root) == "H"
